35ce23b04be68c8cb92bde886325c4ff8d828ae2
Wellochang <wello@startrii.com>
2021-12-01 11:53:15 +0800

    feat(tools): build can_interface make import easily

--- tools/can/can_interface.py
+++ tools/can/can_interface.py
@@ -7,11 +7,6 @@ import logging
 import socket
 import struct
 import sys
-#import time
-#import os
-#import signal
-#from datetime import datetime
-#from logging.handlers import TimedRotatingFileHandler
 
 from bitstring import BitArray 
 
@@ -89,24 +84,4 @@ def parse_args():
 
     return parser.parse_args()
 
-# in subscriber
 args = parse_args()
-sm = messaging.SubMaster(['carControl'])
-while 1:
-  sm.update()
-  throttle_op = clip(sm['carControl'].actuators.accel/1.6, 0.0, 1.0)
-  brake_op = clip(-sm['carControl'].actuators.accel/4.0, 0.0, 1.0)
-  steer_op = sm['carControl'].actuators.steeringAngleDeg
-
-  send_cmd('0x060', 0, brake_op)
-  send_cmd('0x062', 0, throttle_op)
-  steer = (steer_op/360.0*600) + 900
-  send_cmd('0xF513', 1, steer)
-  print("throttle: ", round(throttle_op, 3), "; steer(motor steps): ", round(steer, 3),  "; brake: ", round(brake_op, 3))
-  time.sleep(0.02)
-
-# in publisher
-#pm = messaging.PubMaster(['sensorEvents'])
-#dat = messaging.new_message('sensorEvents', size=1)
-#dat.sensorEvents[0] = {"gyro": {"v": [0.1, -0.1, 0.1]}}
-#pm.send('sensorEvents', dat)
