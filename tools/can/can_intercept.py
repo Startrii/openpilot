9e25b7ea5e8ab64fabe6bac5239a6e2e64b0eabc
KHLee <khlee@startrii.com>
2021-12-07 09:44:57 +0800

    fix(can): read 'can' from can interception

--- tools/can/can_intercept.py
+++ tools/can/can_intercept.py
@@ -1,17 +1,23 @@
 from cereal import log, car
 import cereal.messaging as messaging
+import time
 import struct
 
-sm = messaging.SubMaster(['sendcan', 'carControl'])
+sm = messaging.SubMaster(['can', 'sendcan', 'carControl'])
 
 while True:
     sm.update()
     for can in sm['sendcan']:
+        print(f"sendcan: addr = {can.address}, data = {can.dat}")
         if can.address == 0x60:
             tmp = struct.unpack('ff', can.dat)
-            print(f"can: accel = {tmp[0]}, deg = {tmp[1]}")
+            print(f"sendcan data: accel = {tmp[0]}, deg = {tmp[1]}")
+    for can in sm['can']:
+        print(f"can: addr = {can.address}, data = {can.dat}")
     cc = sm['carControl']
     print(f"accel = {cc.actuators.accel}, deg = {cc.actuators.steeringAngleDeg}")
+    time.sleep(0.01)
+
 
 
 
